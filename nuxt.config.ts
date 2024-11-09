992d22c0ac8a3bc158a085944324b13c4ab83024
MuhammadM1998 <MuhammadM.1097@gmail.com>
2024-11-09 16:17:52 +0200

    chore: add nuxt/i18n module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxt/eslint', '@nuxt/image', '@nuxt/ui'],
+  modules: ['@nuxt/eslint', '@nuxt/image', '@nuxt/ui', '@nuxtjs/i18n'],
 
   // Nuxt ESlint (https://eslint.nuxt.com)
   eslint: { config: { standalone: false } }, // Generate only Nuxt-specific rules as we're using `antfu/eslint-config` for other Vue & TS rules
@@ -11,7 +11,18 @@ export default defineNuxtConfig({
   // Nuxt Color Mode (https://color-mode.nuxtjs.org/#configuration)
   colorMode: { preference: 'light' },
 
+  // Nuxt I18n (https://i18n.nuxtjs.org/docs/getting-started)
+  i18n: {
+    locales: [
+      { code: 'ar', language: 'ar-EG', file: 'ar.json', dir: 'rtl' },
+      { code: 'en', language: 'en-US', file: 'en.json', dir: 'ltr' },
+    ],
+    defaultLocale: 'en',
+    lazy: true,
+  },
+
   // Misc
+  experimental: { typedPages: true },
   future: { compatibilityVersion: 4 },
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
