2b90f0b00aa232623ce4a2cd8bb96b3e2cce93ce
MuhammadM1998 <MuhammadM.1097@gmail.com>
2024-11-09 14:11:38 +0200

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,7 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  // Misc
+  future: { compatibilityVersion: 4 },
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
+});
