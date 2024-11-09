a21493a0d32ff64332b8c426711c422111258fd0
MuhammadM1998 <MuhammadM.1097@gmail.com>
2024-11-09 19:13:54 +0200

    feat: implement header

---
+++ app/components/Layout/Header.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const navLinks = ref([{ label: 'Home', to: '/' }, { label: 'Promo' }, { label: 'My Booking' }])
+</script>
+
+<template>
+  <UContainer as="header" class="pt-8 pb-4 flex items-center justify-between">
+    <LayoutMobileSlideover :items="navLinks" class="md:hidden" />
+
+    <div class="grow md:grow-0">
+      <AppLogo class="mx-auto md:mx-0" />
+    </div>
+
+    <UNavigationMenu :items="navLinks" highlight class="w-fit hidden md:flex" />
+
+    <UButton label="Sign In" class="hidden md:block" />
+  </UContainer>
+</template>
---
+++ app/components/Layout/MobileSlideover.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineProps<{ items: Array<{ label: string }> }>()
+</script>
+
+<template>
+  <USlideover title="Goose Flight" close-icon="ic:sharp-close">
+    <button class="bg-[#DBECFD] text-primary p-2 rounded inline-flex md:hidden">
+      <UIcon name="radix-icons:hamburger-menu" />
+    </button>
+
+    <template #body>
+      <UNavigationMenu :items orientation="vertical" />
+    </template>
+  </USlideover>
+</template>
