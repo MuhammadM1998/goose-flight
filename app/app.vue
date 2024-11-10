aff107e07b59ec107584a1008c60451e051542ba
MuhammadM1998 <MuhammadM.1097@gmail.com>
2024-11-10 21:05:58 +0200

    feat(locale): automatically set html attrs for the selected locale

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,12 @@
+<script setup lang="ts">
+const i18nHead = useLocaleHead()
+useHead({
+  htmlAttrs: () => i18nHead.value.htmlAttrs,
+  link: () => [...(i18nHead.value.link || [])],
+  meta: () => [...(i18nHead.value.meta || [])],
+})
+</script>
+
 <template>
   <UApp>
     <NuxtRouteAnnouncer />
